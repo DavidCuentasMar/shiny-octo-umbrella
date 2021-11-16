f807a54e5bf3c25d6b76dce28fc5a75ebe5fde9a
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2021-11-16 18:37:06 -0500

    jwt

--- app.py
+++ app.py
@@ -1,16 +1,56 @@
 
   
 from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
 from datetime import datetime
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+
 app = Flask(__name__)
 
 client = MongoClient('mongodb://mongodb:27017/')
 db = client.test_database
 
+app.config["JWT_SECRET_KEY"] = "shhhh-super-secret"  
+jwt = JWTManager(app)
+
 @app.route('/')
 def hello_world():
     return "Hello world!"
+
+@app.route('/protected', methods=["GET"])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    user = db.users.find_one({"_id" :ObjectId(current_user_id)})
+    if not user:
+        return 'invalid token', 403
+    return jsonify({"id": str(user.get('_id')), "username": user.get('username')}), 200    
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    data = request.json
+    username = data.get('username')
+    
+    if not username:
+        return 'missing params', 400
+    
+    password = data.get('password')
+    if not password:
+        return 'missing params', 400
+
+    user = db.users.find_one(
+        {
+            'username':username,
+            'password':password
+        }
+    )
+    if not user:
+        return 'wrong credentials', 403
+
+    access_token = create_access_token(identity=str(user.get('_id')))
+    return jsonify({ "token": access_token, "user_id": str(user.get('_id') )})
 
 @app.route('/users')
 def users():
@@ -37,11 +77,11 @@ def user():
     username = data.get('username')
     
     if not username:
-        return 'missing params', 403
+        return 'missing params', 400
     
     password = data.get('password')
     if not password:
-        return 'missing params', 403
+        return 'missing params', 400
 
     user = db.users.find_one({'username':username})
     if user:
@@ -54,9 +94,11 @@ def user():
         'created_at': now,
         'updated_at': now
     }
+
     """
     to-do: Do not store plain text password
     """
+
     db.users.insert_one(user)
 
     return 'Saved!', 201
